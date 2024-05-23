42f1fcc801fafb95801843743329835ee9d99141
ArunKarthik05 <akkarthikk2002@gmail.com>
2024-05-23 17:38:39 +0530

    contactus changes

---
+++ components/contact-us/contact.js
@@ -0,0 +1,67 @@
+import Image from "next/image";
+import styles from './contact.module.scss';
+import { CustomButton } from "../button/button";
+
+export const Contact=() =>{
+  const contactUs = [
+    {
+      image :"/contact-us/map.png",
+      heading: "Meet Us In Office",
+      text : "2nd Floor, Elysium Towers, Gopalapuram, Coimbatore.",
+    },
+    {
+      image :"/contact-us/call.png",
+      heading: "Contact Us At",
+      text : "+91 9363807080",
+    },
+    {
+      image :"/contact-us/mail.png",
+      heading: "Email Us At Us",
+      text : "Upliftedutech@gmail.com",
+    }
+  ];
+
+  const form = ["Full Name", "Phone Number", "Message"];
+  return (
+    <div className={styles.gradientContainer}>
+        <div className={styles.leftContainer}>
+           <h3 className={styles.contactUsHeading}>Let's Uplift Your Career</h3>
+
+            {contactUs.map((item,index)=>{
+              return (
+                <div className={styles.rowContainer} key={index}>
+                  <div className={styles.iconContainer}>
+                    <Image src={`${item.image}`} alt={`${item.heading}`} height={40} width={40}/>
+                  </div>
+                  <div>
+                    <h3>{item.heading}</h3>
+                    <p>{item.text}</p>
+                  </div>
+                </div>
+              )
+            })}
+        </div>
+
+        <div className={styles.rightContainer}>
+          <div className={styles.innerContainer}>
+            <h3 className={styles.heading}>Let's Connect</h3>
+            
+            {form.map((el,inx)=>{
+              return (
+                <div className={styles.formContainer} key={inx}>
+                  <h3>{el}<span>*</span></h3>
+                  <input></input>
+                </div>
+              )
+            })
+          }
+            <div className={styles.btnContainer}>
+              <CustomButton text="Submit" bgColor="white" textColor="black" hoverColor="white"/>
+            </div>
+          </div>
+
+        </div>
+
+    </div>
+  )
+}
---
+++ components/courses/courses.js
@@ -0,0 +1,62 @@
+"use client"
+import React, { useState } from 'react';
+import Image from 'next/image'
+import styles from "./courses.module.scss";
+import { CustomButton } from "@/components/button/button";
+
+export const Courses = ({image,courseName,courseDetails,hoverDetails})=> {
+    const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  return (
+        <div className={styles.gradientContainer} onMouseLeave={handleMouseLeave} style={{ backgroundColor: isHovered && '#FF3364' }}>
+            <div className={styles.contentContainer}>
+                {!isHovered ? (
+                <>
+                <div className={styles.imgContainer}>
+                <Image src={`/courses/${image}`} alt={courseName} height={400} width={400}></Image>
+                </div>
+                <div className={styles.courseName}><h1>{courseName}</h1></div>
+                <div className={styles.courseDetails}><p>{courseDetails}</p></div>
+                <div className={styles.btnContainer} onClick={handleMouseEnter}><CustomButton text="Know More" bgColor="black" hoverColor="rgb(61, 60, 60)" hoverTextColor="white"/></div>
+                </>
+                ):(
+                    <>
+                      <div className={styles.hoverDiv}>
+                        <p className={styles.hoverDetails}>{hoverDetails}</p>
+                      </div>
+                      <div className={styles.btnContainer}><CustomButton text="Enroll Now" bgColor="black" textColor="white" hoverColor="rgb(61, 60, 60)" hoverTextColor="white"/></div>
+                    </>
+                )
+                }
+            </div>
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
