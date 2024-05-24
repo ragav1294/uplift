f7d03ce0782e4a1b842e73fbaab9fcb389eda4e2
ArunKarthik05 <akkarthikk2002@gmail.com>
2024-05-24 22:50:49 +0530

    courses div changes

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 import styles from './navbar.module.scss';
 import { Hamburger } from '@/components/Hamburger/hammenu';
 
 export const Navbar = ({ refs }) => {
-  const handleScroll = (ref) => {
+  const [isSticky, setIsSticky] = useState(false);
+  const navbarRef = useRef(null);
+
+  const handleScroll = () => {
+    if (window.scrollY > 0) {
+      setIsSticky(true);
+    } else {
+      setIsSticky(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const handleScrollToSection = (ref) => {
     if (ref.current) ref.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
   };
 
   return (
-    <div className={styles.navMain}>
+    <div className={`${styles.navMain} ${isSticky && styles.sticky}`} ref={navbarRef}>
       <div className={styles.navLeft}>
         <div>
           <Image src="/Uplift-logo.svg" alt="Uplift Logo" width={100} height={100} />
@@ -18,14 +36,14 @@ export const Navbar = ({ refs }) => {
       </div>
 
       <div className={styles.navRight}>
-            <Hamburger refs={refs}/>
-          <ul className={styles.desktop}>
-            <li onClick={() => handleScroll(refs.homeRef)}>Home</li>
-            <li onClick={() => handleScroll(refs.servicesRef)}>Services</li>
-            <li onClick={() => handleScroll(refs.coursesRef)}>Courses</li>
-            <li onClick={() => handleScroll(refs.FAQRef)}>FAQ</li>
-            <li onClick={() => handleScroll(refs.ContactRef)}>Contact Us</li>
-          </ul>
+        <Hamburger refs={refs} />
+        <ul className={styles.desktop}>
+          <li onClick={() => handleScrollToSection(refs.homeRef)}>Home</li>
+          <li onClick={() => handleScrollToSection(refs.servicesRef)}>Services</li>
+          <li onClick={() => handleScrollToSection(refs.coursesRef)}>Courses</li>
+          <li onClick={() => handleScrollToSection(refs.FAQRef)}>FAQ</li>
+          <li onClick={() => handleScrollToSection(refs.ContactRef)}>Contact Us</li>
+        </ul>
       </div>
     </div>
   );
