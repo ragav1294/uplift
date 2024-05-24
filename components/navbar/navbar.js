da9b5983a87c2d37fbccdd0079f21abf9bef5ddc
ArunKarthik05 <akkarthikk2002@gmail.com>
2024-05-24 15:37:47 +0530

    responsiveness added

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import styles from './navbar.module.scss';
-import HamburgerMenu from './HamburgerMenu';
+import { HamburgerMenu } from './HamburgerMenu';
 
 export const Navbar = ({ refs }) => {
   const handleScroll = (ref) => {
